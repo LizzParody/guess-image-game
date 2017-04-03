e378f9754a888ffda56fd926ef1c31ebc19654b5
Lyzzeth Parody Álvarez <lizparody@gmail.com>
2017-04-03 10:34:58 -0500

    added the function using closure

--- blurryImages.js
+++ blurryImages.js
@@ -21,3 +21,28 @@ function reblur(image) {
 }
 
 window.onload = init;
+
+//Using closure
+
+/*function init() {
+  var images = document.getElementsByTagName("img");
+   for (var i = 0; i < images.length; i++) {
+     images[i].onclick = showAnswer;
+    }
+  }
+
+function showAnswer(eventObj) {
+  var image = eventObj.target;
+  var name = image.id;
+  name = name + ".jpg";
+  image.src = name;
+
+  setTimeout(function reblur() {
+  var name = image.id;
+  name = name + "blur.jpg";
+  image.src = name;
+  }, 2000);
+}
+
+window.onload = init;
+*/
